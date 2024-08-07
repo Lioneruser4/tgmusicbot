3f7f79748adb7ff5dc8c298fd18713a107879375
Lioneruser4 <117773578+Lioneruser4@users.noreply.github.com>
2024-08-07 20:48:17 +0400

    Update bot.py

--- bot.py
+++ bot.py
@@ -48,7 +48,7 @@ REGEX_EXCLUDE_URL = (
 
 def get_music_chats():
     chats = []
-    for x in os.environ["MUSIC_CHATS"].split("-1001158319567"):
+    for x in os.environ["MUSIC_CHATS"].split(" "):
         try:
             chats.append(int(x))
         except ValueError:
